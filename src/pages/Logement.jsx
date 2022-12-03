5b4f877786479df2f76f8ff7edd8ad2222e9d98a
Kiggz <nico.peignot@outlook.fr>
2022-12-04 00:40:53 +0100

    wip

--- src/pages/Logement.jsx
+++ src/pages/Logement.jsx
@@ -3,6 +3,8 @@ import Slideshow from "../components/Slideshow"
 import Dropdown from "../components/Dropdown"
 import Tag from "../components/Tag"
 
+import React, { useState, useEffect } from "react";
+
 import { useParams } from "react-router-dom";
 
 
@@ -11,13 +13,27 @@ export default function Logement(logementData) {
 
   const { id } = useParams();
   console.log(id)
+  
+  const gallery = []
 
+  const [logement, setLogement] = useState([]);
+
+  useEffect(() => {
+    const fetchDate = async () => {
+      const res = await fetch("http://127.0.0.1:5173/logement.json");
+      const json = await res.json();
+      const data = json.filter((e) => e.id == id)
+      console.log(data);
+      setLogement(data);
+    };
+    fetchDate();
+  }, []);
 
 
 
   return (
     <div className="logementContainer">
-
+      <Slideshow gallery={gallery} />
     </div>
   )
 }
